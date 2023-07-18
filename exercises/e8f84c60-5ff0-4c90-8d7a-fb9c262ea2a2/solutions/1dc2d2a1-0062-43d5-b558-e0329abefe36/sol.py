e29149d0f46ff03b02855c010265e9539b631323
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 13:12:01 +0100

    created file exercises/e8f84c60-5ff0-4c90-8d7a-fb9c262ea2a2/solutions/1dc2d2a1-0062-43d5-b558-e0329abefe36/sol.py

---
+++ exercises/e8f84c60-5ff0-4c90-8d7a-fb9c262ea2a2/solutions/1dc2d2a1-0062-43d5-b558-e0329abefe36/sol.py
@@ -0,0 +1,18 @@
+def zakoduj_powtorzenia(napis):
+    zakodowany_napis = ""
+    count = 1
+
+    for i in range(1, len(napis)):
+        if napis[i] == napis[i - 1]:
+            count += 1
+        else:
+            if count > 1:
+                zakodowany_napis += str(count)
+            zakodowany_napis += napis[i - 1]
+            count = 1
+
+    if count > 1:
+        zakodowany_napis += str(count)
+    zakodowany_napis += napis[-1]
+
+    return zakodowany_napis
