7f704aac11f35ed43c515c1ae0b423bc229749c5
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 12:08:32 +0100

    updated file exercises/63c3b740-5668-4294-a721-2ba611b1c2e2/solutions/c0f0155d-f7c3-485a-a97d-b121b9c69d08/sol.py

--- exercises/63c3b740-5668-4294-a721-2ba611b1c2e2/solutions/c0f0155d-f7c3-485a-a97d-b121b9c69d08/sol.py
+++ exercises/63c3b740-5668-4294-a721-2ba611b1c2e2/solutions/c0f0155d-f7c3-485a-a97d-b121b9c69d08/sol.py
@@ -1,6 +1,6 @@
 def zlicz_wyrazy_duza_litera(tekst):
     wyrazy = tekst.split()
-    liczba_wyrazow_duza_litera = sum(1 for wyraz in wyrazy if wyraz.isupper())
-    print(liczba_wyrazow_duza_litera)
+    liczba_wyrazow_duza_litera = sum(1 for wyraz in wyrazy if any(litera.isupper() for litera in wyraz))
+    return liczba_wyrazow_duza_litera
 
 print(zlicz_wyrazy_duza_litera(input()))
