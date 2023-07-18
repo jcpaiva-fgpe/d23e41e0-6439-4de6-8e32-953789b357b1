54d0e60855a0ef2569e83a0e402769da6b071d15
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 15:03:51 +0100

    created file exercises/8841eac6-9683-4d7c-abad-eeaaadef8125/skeletons/cecf780d-c42e-4e58-acca-894bbb1b75e2/s.py

---
+++ exercises/8841eac6-9683-4d7c-abad-eeaaadef8125/skeletons/cecf780d-c42e-4e58-acca-894bbb1b75e2/s.py
@@ -0,0 +1,19 @@
+def najdluzsza_wspolna_sekwencja(*napisy):
+    # Pod żadnym pozorem nie usuwaj poniższego kodu. Rozszerz funkcję o znajdowanie wspólnej sekwencji znaków.
+    # Tutaj umieść swój kod
+
+
+
+
+# Oto niezbedne przykłady użycia funkcji, testujace Twoj kod. NIE ZMIENIAĆ!
+przyklad1 = najdluzsza_wspolna_sekwencja("a", "b", "c")
+przyklad2 = najdluzsza_wspolna_sekwencja("rok", "bok", "ćwok")
+przyklad3 = najdluzsza_wspolna_sekwencja("dzisiejszy", "dzisiaj", "niedzisiejszy")
+przyklad4 = najdluzsza_wspolna_sekwencja("pogoda", "niepogoda", "pagoda", "wygoda", "zagroda", "broda", "oda", "woda")
+przyklad5 = najdluzsza_wspolna_sekwencja("pogoda", "niepogoda", "x")
+
+print(przyklad1)
+print(przyklad2)
+print(przyklad3)
+print(przyklad4)
+print(przyklad5)
