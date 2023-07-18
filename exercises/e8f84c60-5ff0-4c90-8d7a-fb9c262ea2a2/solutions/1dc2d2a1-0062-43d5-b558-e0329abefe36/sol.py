55fc6c2f24cfc6a290dc958d5f5af1beb14bf1a7
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 13:24:51 +0100

    updated file exercises/e8f84c60-5ff0-4c90-8d7a-fb9c262ea2a2/solutions/1dc2d2a1-0062-43d5-b558-e0329abefe36/sol.py

--- exercises/e8f84c60-5ff0-4c90-8d7a-fb9c262ea2a2/solutions/1dc2d2a1-0062-43d5-b558-e0329abefe36/sol.py
+++ exercises/e8f84c60-5ff0-4c90-8d7a-fb9c262ea2a2/solutions/1dc2d2a1-0062-43d5-b558-e0329abefe36/sol.py
@@ -2,18 +2,20 @@ def zakoduj_powtorzenia(napis):
     zakodowany_napis = ""
     count = 1
 
-    for i in range(1, len(napis)):
-        if napis[i] == napis[i - 1]:
+    for i in range(len(napis) - 1):
+        if napis[i] == napis[i + 1]:
             count += 1
         else:
             if count > 1:
-                zakodowany_napis += str(count)
-            zakodowany_napis += napis[i - 1]
+                zakodowany_napis += napis[i] + str(count - 1)
+            else:
+                zakodowany_napis += napis[i]
             count = 1
 
     if count > 1:
-        zakodowany_napis += str(count)
-    zakodowany_napis += napis[-1]
+        zakodowany_napis += napis[-1] + str(count)
+    else:
+        zakodowany_napis += napis[-1]
 
     return zakodowany_napis
 
