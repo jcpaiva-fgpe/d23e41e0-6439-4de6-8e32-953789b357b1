39056d33c4f8a9178878b0cea7c6f2bf21dccdde
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 13:31:52 +0100

    created file exercises/0da930bc-cc6c-4da5-baf3-100d498c85c7/solutions/22a8d59d-7d8d-42f9-a79b-e3cedb6b6a37/sol.py

---
+++ exercises/0da930bc-cc6c-4da5-baf3-100d498c85c7/solutions/22a8d59d-7d8d-42f9-a79b-e3cedb6b6a37/sol.py
@@ -0,0 +1,13 @@
+def uproszcz_napis(napis):
+    polskie_znaki = "ĄĆĘŁŃÓŚŹŻąćęłńóśźż"
+    uproszczenia = "ACELNOSZZacelnoszz"
+
+    napis_uproszczony = ""
+    for znak in napis:
+        if znak in polskie_znaki:
+            indeks = polskie_znaki.index(znak)
+            napis_uproszczony += uproszczenia[indeks]
+        else:
+            napis_uproszczony += znak
+
+    return napis_uproszczony
