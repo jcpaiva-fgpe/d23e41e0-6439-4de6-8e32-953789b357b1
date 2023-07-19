1cd9ab94d5ce863cb033c28abe7dd5d88849ab87
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-19 07:15:50 +0100

    updated file exercises/e68276d9-2be2-465c-a900-d97c0a9787f4/skeletons/6e0fa079-0ccf-4581-bb2c-a508bafe84c4/s.py

--- exercises/e68276d9-2be2-465c-a900-d97c0a9787f4/skeletons/6e0fa079-0ccf-4581-bb2c-a508bafe84c4/s.py
+++ exercises/e68276d9-2be2-465c-a900-d97c0a9787f4/skeletons/6e0fa079-0ccf-4581-bb2c-a508bafe84c4/s.py
@@ -1,3 +1,5 @@
+# Wywołując funkcję generującą tabele obowiazkowo użyj zmiennej input_data.
+# Nie zmieniaj danych!
 input_data = [["Rok", "Mistrz"], [1986, "Argentyna"], [1990, "Niemcy"], [1994, "Brazylia"],
               [1998, "Francja"], [2002, "Brazylia"], [2006, "Włochy"], [2010, "Hiszpania"],
               [2014, "Niemcy"], [2018, "Francja"], [2022, "Argentyna"]]
