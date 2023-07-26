0d8e34d5d4344f023cab08a16534bb4b5ea98a8e
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-26 19:37:49 +0100

    updated file exercises/03ba6d96-6017-40ae-8c89-1c16d044b497/solutions/fe8db550-f1e4-4db2-a08d-cdde9af83b54/sol.py

--- exercises/03ba6d96-6017-40ae-8c89-1c16d044b497/solutions/fe8db550-f1e4-4db2-a08d-cdde9af83b54/sol.py
+++ exercises/03ba6d96-6017-40ae-8c89-1c16d044b497/solutions/fe8db550-f1e4-4db2-a08d-cdde9af83b54/sol.py
@@ -1,5 +1,5 @@
 print("Pomyśl liczbę od 1 do 99 i wciśnij enter.")
-input()
+w = input()
 
 dolna_granica = 1
 gorna_granica = 99
@@ -7,11 +7,10 @@ liczba_prob = 0
 
 while True:
    
-
     liczba_prob += 1
     propozycja = (dolna_granica + gorna_granica) // 2
 
-    print(f"Czy to {propozycja}"')
+    print(f"Czy to {propozycja}?")
     odpowiedz = input()
 
     if odpowiedz == "-":
