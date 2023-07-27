55f5a1ea38f07019414fc3ed4dd880e79722e514
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-27 12:18:46 +0100

    updated file exercises/dfd0fe60-29b6-421f-8dc7-7659fd787586/solutions/96a80d78-056c-44c5-975b-949642bea6b5/sol.py

--- exercises/dfd0fe60-29b6-421f-8dc7-7659fd787586/solutions/96a80d78-056c-44c5-975b-949642bea6b5/sol.py
+++ exercises/dfd0fe60-29b6-421f-8dc7-7659fd787586/solutions/96a80d78-056c-44c5-975b-949642bea6b5/sol.py
@@ -2,7 +2,8 @@ def konwertuj_szesnastkowy(napis):
     if napis.startswith("$") and all(c in "0123456789abcdef" for c in napis[1:]):
         szesnastkowy = napis[1:]
         szesnastkowy = szesnastkowy.zfill(4)
-        return "0x" + szesnastkowy
+        r = "0x" + szesnastkowy
+        return r
 
     return napis
 
